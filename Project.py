e56358b4afd58b1ba7b148a68a3667b8350ef35a
MendozaProject <themendozaproject@gmail.com>
2016-09-23 13:20:21 -0700

    Adjusted for Project class variables

--- Project.py
+++ Project.py
@@ -3,14 +3,13 @@ from datetime import date
 
 class Project:
 
-    tasks = []
-    categories = []
-
     def __init__(self, name="New Project", description="Add description"):
         self.created_date = date.strfdate("%x")
         self.name = name
         self.description = description
+        self.tasks = []
+        self.categories = []
 
-    def edit_Project(new_name, new_description):
-        name = new_name
-        description = new_description
+    def edit_Project(self, new_name, new_description):
+        self.name = new_name
+        self.description = new_description
