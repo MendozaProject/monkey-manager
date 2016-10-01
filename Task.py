346bc52317d3188ee7756c5601847433601c53dc
BrittDibble <dibbl001@cougars.csusm.edu>
2016-09-30 23:42:18 -0700

    Removed semicolens from Task and added id_number field to class

--- Task.py
+++ Task.py
@@ -3,8 +3,8 @@ from datetime import date
 
 class Task:
 
-    def __init__(self, name="New Task", due_date=date.max, description="New Description"):
-        self.name = name;
-        self.due_date = due_date;
-        self.description =  description;
-
+    def __init__(self, name="New Task", due_date=date.max, description="New Description", id_number=0):
+        self.name = name
+        self.due_date = due_date
+        self.description = description
+        self.id = id_number
