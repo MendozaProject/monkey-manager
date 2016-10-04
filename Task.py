4be790c9d6058499d5bcbd77ee4c4631c43df321
BrittDibble <dibbl001@cougars.csusm.edu>
2016-10-04 15:45:32 -0700

    Added when edited comment at top of file

--- Task.py
+++ Task.py
@@ -1,16 +1,20 @@
+# Last Edited on 10/4/2016 by Brittany Dibble.
+# Functions edited: __init__ and edit_task
 from datetime import date
 
 
 class Task:
 
-    def __init__(self, name="New Task", due_date=date.max, description="New Description", id_number=0, is_complete=False):
+    def __init__(self, name="New Task", due_date=date.max,
+                 description="New Description", id_number=0, is_complete=False):
         self.name = name
         self.due_date = due_date
         self.description = description
         self.id_number = id_number
         self.is_complete = is_complete
 
-    def edit_task(self, name="New Name", due_date=date.max, description="New Description", id_number=0, is_complete=False):
+    def edit_task(self, name="New Name", due_date=date.max,
+                  description="New Description", id_number=0, is_complete=False):
         self.name = name
         self.due_date = due_date
         self.description = description
