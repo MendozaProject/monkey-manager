18d65643205a75f7d818ec956a460356367c8f00
BrittDibble <dibbl001@cougars.csusm.edu>
2016-10-03 22:48:29 -0700

    Added edit_task() method and is_complete variable

--- Task.py
+++ Task.py
@@ -3,8 +3,16 @@ from datetime import date
 
 class Task:
 
-    def __init__(self, name="New Task", due_date=date.max, description="New Description", id_number=0):
+    def __init__(self, name="New Task", due_date=date.max, description="New Description", id_number=0, is_complete=False):
         self.name = name
         self.due_date = due_date
         self.description = description
-        self.id = id_number
+        self.id_number = id_number
+        self.is_complete = is_complete
+
+    def edit_task(self, name="New Name", due_date=date.max, description="New Description", id_number=0, is_complete=False):
+        self.name = name
+        self.due_date = due_date
+        self.description = description
+        self.id_number = id_number
+        self.is_complete = is_complete
