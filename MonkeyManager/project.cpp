8d6b55b8eb47aa896b6940d8ea95ce346ea2bb44
Greg Farinelli <gregfarinelli@gmail.com>
2016-11-29 08:44:09 -0800

    refactored project class

--- MonkeyManager/project.cpp
+++ MonkeyManager/project.cpp
@@ -1,7 +1,5 @@
 #include "project.h"
 
-#include <QDebug>
-
 Project::Project(){
     m_name = "New Project";
     m_description = "Add description";
@@ -22,7 +20,12 @@ Project::~Project(){
 }
 
 // task management functions
-vector<Task>::iterator Project::find_task_by_name(string name){
+/**
+ * @brief Project::find_task_by_name - finds a task in the vector by name
+ * @param name
+ * @return
+ */
+vector<Task>::iterator Project::find_task_by_name(string name) {
     bool found = false;
     for (m_tasks_iterator = m_tasks.begin(); m_tasks_iterator != m_tasks.end(); m_tasks_iterator++){
         if ((*m_tasks_iterator).get_name() == name){
@@ -36,25 +39,43 @@ vector<Task>::iterator Project::find_task_by_name(string name){
     return m_tasks_iterator;
 }
 
+/**
+ * @brief Project::remove_task - removes a task in the task vector by index
+ * @param index
+ */
 void Project::remove_task(vector<Task>::iterator index) {
     for (m_tasks_iterator = m_tasks.begin(); m_tasks_iterator != index; m_tasks_iterator++){;}
     m_tasks.erase(m_tasks_iterator);
 }
 
+/**
+ * @brief Project::add_task - add a new task to the vector
+ * @param new_task
+ */
+
 void Project::add_task(Task new_task){
     new_task.set_task_number(assign_ticket());
     m_tasks.push_back(new_task);
-    qDebug() << "From Project add task: " + QString::fromStdString(to_string(m_tasks.size()));
 }
+
+/**
+ * @brief Project::assign_ticket - assigns a new ticket number to a task
+ * @return
+ */
 
 int Project::assign_ticket(){
     return m_current_ticket++;
 }
 
+/**
+ * @brief Project::editTask - set a tasks properties via the task passed in
+ * @param task_name
+ * @param r_task
+ */
+
 void Project::editTask(string task_name, Task r_task)
 {
     vector<Task>::iterator task = find_task_by_name(task_name);
-    //  ^^^ this is the kind of stuff we should be doing
 
     task->set_name( r_task.get_name() );
     task->set_due_date( r_task.get_due_date() );
@@ -65,25 +86,56 @@ void Project::editTask(string task_name, Task r_task)
 }
 
 // setter functions
+
+/**
+ * @brief Project::set_created_date - set the date created for a task
+ * @param created_date
+ */
 void Project::set_created_date(QDate created_date){
     m_created_date = created_date;
 }
+
+/**
+ * @brief Project::set_name - set the name of a task
+ * @param name
+ */
 
 void Project::set_name(string name){
     m_name = name;
 }
 
+/**
+ * @brief Project::set_description - set the description of a task
+ * @param description
+ */
+
 void Project::set_description(string description){
     m_description = description;
 }
 
+/**
+ * @brief Project::set_tasks - sets the project tasks to the ones passed in
+ * @param tasks
+ */
+
 void Project::set_tasks(vector<Task> tasks){
     m_tasks = tasks;
 }
+
+/**
+ * @brief Project::set_current_ticket - assigns current ticket number
+ * @param ticket_number
+ */
+
 void Project::set_current_ticket(int ticket_number)
 {
     m_current_ticket=ticket_number;
 }
+
+/**
+ * @brief Project::set_selected_ticket - sets the selected ticket number
+ * @param ticket_number
+ */
 
 void Project::set_selected_ticket(int ticket_number)
 {
@@ -91,30 +143,49 @@ void Project::set_selected_ticket(int ticket_number)
 }
 
 // getter functions
+/**
+ * @brief Project::get_created_date - get created date of a task
+ * @return
+ */
 QDate Project::get_created_date(){
     return m_created_date;
 }
+
+/**
+ * @brief Project::get_name - get a name of a task
+ * @return
+ */
 
 string Project::get_name() const {
     return m_name;
 }
 
+/**
+ * @brief Project::get_description - get a description of the task
+ * @return
+ */
+
 string Project::get_description(){
     return m_description;
 }
 
+/**
+ * @brief Project::get_tasks - get tasks for a current project
+ * @return
+ */
+
 vector<Task> Project::get_tasks(){
     return m_tasks;
-    qDebug() << "From Project get tasks: " + QString::fromStdString(to_string(m_tasks.size()));
 }
+
+/**
+ * @brief Project::get_current_ticket - get current ticket for a project
+ * @return
+ */
 
 int Project::get_current_ticket()
 {
     return m_current_ticket;
 }
 
-int Project::get_selected_ticket()
-{
-    return m_selected_ticket;
-}
 
--- MonkeyManager/project.h
+++ MonkeyManager/project.h
@@ -28,27 +28,89 @@ class Project {
         ~Project();
         
         // task management functions
+        /**
+         * @brief find_task_by_name - finds a task in the vector by name
+         * @param name
+         * @return Task vector iterator
+         */
         vector<Task>::iterator find_task_by_name(string name);
+        /**
+         * @brief remove_task - removes a task in the task vector by index
+         * @param index
+         */
         void remove_task(vector<Task>::iterator index);
+        /**
+         * @brief add_task - add a new task to the vector
+         * @param new_task
+         */
         void add_task(Task new_task);
+        /**
+         * @brief assign_ticket - assigns a new ticket number to a task
+         * @return int
+         */
         int assign_ticket();
+        /**
+         * @brief editTask - set a tasks properties via the task passed in
+         */
         void editTask(string, Task);
         
         // setter functions
+        /**
+         * @brief set_created_date - set the date created for a task
+         * @param created_date
+         */
         void set_created_date(QDate created_date);
+        /**
+         * @brief set_name - set the name of a task
+         * @param name
+         */
         void set_name(string name);
+        /**
+         * @brief set_description - set the description of a task
+         * @param description
+         */
         void set_description(string description);
+        /**
+         * @brief set_tasks - sets the project tasks to the ones passed in
+         * @param tasks
+         */
         void set_tasks(vector<Task> tasks);
+        /**
+         * @brief set_current_ticket - assigns current ticket number
+         * @param ticket_number
+         */
         void set_current_ticket(int ticket_number);
+        /**
+         * @brief set_selected_ticket - sets the selected ticket number
+         */
         void set_selected_ticket(int);
         
         // getter functions
+        /**
+         * @brief get_created_date - get created date of a task
+         * @return
+         */
         QDate get_created_date();
+        /**
+         * @brief get_name - get a name of a task
+         * @return QDate
+         */
         string get_name() const;
+        /**
+         * @brief get_description - get a description of the task
+         * @return string
+         */
         string get_description();
+        /**
+         * @brief get_tasks - get tasks for a current project
+         * @return string
+         */
         vector<Task> get_tasks();
+        /**
+         * @brief get_current_ticket - get current ticket for a project
+         * @return string
+         */
         int get_current_ticket();
-        int get_selected_ticket();
 };
 
 #endif
