e06cffd34d6c0fb5fa33b396032ce0f57bf6c087
NICK AUBUCHON <npaubuchon@gmail.com>
2016-11-29 18:22:11 -0800

    trying again

--- MonkeyManager/mainwindow.cpp
+++ MonkeyManager/mainwindow.cpp
@@ -1,23 +1,12 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include <QDebug>
-#include <QDesktopWidget>
-#include <QLabel>
-#include <QPalette>
-#include <QFontDatabase>
-#include <QFont>
-#include <QPushButton>
-#include "taskwidget.h"
-#include "taskdialog.h"
-#include "project.h"
-#include "json_utils.h"
+
 static MainWindow *s_pMainWindow = NULL;
 
-MainWindow *MainWindow::getInstance()
-{
-    return s_pMainWindow;
-}
-
+///Public Functions
+/**
+ * @brief Constructor
+ * @param parent : QT default
+ */
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -30,15 +19,15 @@ MainWindow::MainWindow(QWidget *parent) :
     newProjectButton = ui->newProjectButton;
     deleteProjectButton= ui->deleteProjectButton;
 
-    ProjectUtils::instance()->set_projects(load_all_projects());
-    if (ProjectUtils::instance()->get_projects().empty()){
-        ProjectUtils::instance()->add_project(Project());
+    ProjectUtils::Instance()->set_projects(load_all_projects());
+    if (ProjectUtils::Instance()->get_projects().empty()){
+        ProjectUtils::Instance()->add_project(Project());
     }
-    ProjectUtils::instance()->set_open_project(ProjectUtils::instance()->get_projects()[0].get_name());
-    m_project = ProjectUtils::instance()->get_open_project();
+    ProjectUtils::Instance()->open_project(ProjectUtils::Instance()->get_projects()[0].get_name());
+    m_project = ProjectUtils::Instance()->get_open_project();
 
     projectListView = ui->projectsList;
-    projectModel = new ProjectListModel(ProjectUtils::instance()->get_projects());
+    projectModel = new ProjectListModel(ProjectUtils::Instance()->get_projects());
     projectListView->setModel(projectModel);
     projectListView->setCurrentIndex(projectModel->index(0,0));
 
@@ -55,7 +44,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     taskDescriptionWidget = ui->taskDescriptionWidget;
     taskDescriptionWidget->hide();
-    update_ui();
+
     //Connections
     connect(newProjectButton, SIGNAL (released()), this, SLOT(onNewProjectButtonClick()));
     connect(deleteProjectButton, SIGNAL(released()), this, SLOT(onDeleteProjectButtonClick()));
@@ -67,6 +56,26 @@ MainWindow::MainWindow(QWidget *parent) :
 
 }
 
+/**
+ * @brief returns the instance of MainWindow
+ * @return
+ */
+MainWindow *MainWindow::getInstance()
+{
+    return s_pMainWindow;
+}
+
+/**
+ * @brief Deconstructor
+ */
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+/**
+ * @brief Formats the frame based on the window size
+ */
 void MainWindow::centerAndResize()
 {
     // get the dimension available on this screen
@@ -87,169 +96,25 @@ void MainWindow::centerAndResize()
     );
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
-void MainWindow::onNewProjectButtonClick()
-{
-    qDebug() << "Main Window New Project Button";
-    Project newProject("New Project", "");
-    projectModel->add_project(newProject, Qt::EditRole);
-}
-
-void MainWindow::onDeleteProjectButtonClick()
-{
-    qDebug() << "Main Window Delete Project Button";
-    QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
-    if(ProjectUtils::instance()->get_projects().empty())
-        return;
-    ProjectUtils::instance()->set_current_project_index(index.first().row());
-    remove_json_project(ProjectUtils::instance()->get_projects().at(ProjectUtils::instance()->get_current_project_index()).get_name());
-    projectModel->delete_project(projectListView->selectionModel()->selectedIndexes().first(), Qt::EditRole);
-    projectListView->setCurrentIndex(projectModel->index(0,0));
-    update_ui();
-}
-
-void MainWindow::onNewTaskButtonClicked()
-{
-    qDebug() << " Main Window Add New Task Button";
-    QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
-    if(ProjectUtils::instance()->get_projects().empty())
-        return;
-    ProjectUtils::instance()->set_current_project_index(index.first().row());
-    setEditFlag(false);
-    taskDialog = new TaskDialog();
-    taskDialog->show();
-}
-
-void MainWindow::onEditTaskButtonClicked()
-{
-    qDebug() << " Main Window Edit Task Button";
-    QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
-    if(ProjectUtils::instance()->get_projects().empty())
-        return;
-    ProjectUtils::instance()->set_current_project_index(index.first().row());
-    //ProjectUtils::Instance()->get_open_task();
-    setEditFlag(true);
-    taskDialog = new TaskDialog();
-    taskDialog->show();
-}
-
-void MainWindow::onDeleteTaskButtonClicked()
-{
-    qDebug() << " Main Window Delete Task Button";
-    QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
-    if(ProjectUtils::instance()->get_projects().empty())
-        return;
-    ProjectUtils::instance()->set_current_project_index(index.first().row());
-    string temp_name = ProjectUtils::instance()->get_open_task().get_name();
-    vector<Task>::iterator temp_index = ProjectUtils::instance()->get_projects().at(index.first().row()).find_task_by_name(temp_name);
-    ProjectUtils::instance()->get_projects().at(index.first().row()).remove_task(temp_index);
-
-    update_ui();
-}
-
-void MainWindow::onTaskDialogAccepted()
-{
-    TaskWidget *task = new TaskWidget;
-    Task current = ProjectUtils::instance()->get_open_task();
-    if(current.get_status() == "To Do")
-        ui->Todo_List->addWidget(task);
-    else if(current.get_status() == "Doing")
-        ui->Doing_List->addWidget(task);
-    else if(current.get_status() == "Testing")
-        ui->Testing_List->addWidget(task);
-    else if(current.get_status() == "Done")
-        ui->Done_List->addWidget(task);
-
-    update_ui();
-}
-
-void MainWindow::DisplayDetailedView(Task task) {
-    ui->taskName->setText(QString::fromStdString(task.get_name()));
-    ui->taskDueDate->setText(task.get_due_date().toString("MM/dd/yyyy"));
-    ui->taskCreatedDate->setText(task.get_created_date().toString("MM/dd/yyyy"));
-    ui->taskDescription->setText(QString::fromStdString(task.get_description()));
-    taskDescriptionWidget->show();
-}
-
-bool MainWindow::getEditFlag()
-{
-    return editingTask;
-}
-
-void MainWindow::setEditFlag(bool f)
-{
-    editingTask = f;
-}
-
-void MainWindow::mousePressEvent(QMouseEvent *event){
-    qDebug() << "Main Window Mouse";
-}
-
-void MainWindow::item_selected_in_list(){
-    qDebug() << "Item Selected in QListView";
-    update_ui();
-}
-
-void MainWindow::remove_all_widgets(QBoxLayout* layout){
-    QLayoutItem *child;
-    while ((child = layout->takeAt(0)) != 0) {
-        delete child->widget();
-        delete child;
-    }
-}
-
-void MainWindow::on_saveProjectsButton_clicked()
-{
-    qDebug() << "Main Window Save All Button";
-    if(ProjectUtils::instance()->get_projects().empty())
-        return;
-    //qDebug() << QString::fromStdString(to_string(ProjectUtils::Instance()->get_projects()[0].get_tasks().size()));
-    //qDebug() << QString::fromStdString(to_string(ProjectUtils::Instance()->get_open_project().get_tasks().size()));
-    save_all_projects(ProjectUtils::instance()->get_projects());
-}
-
-void MainWindow::on_saveCurrentProjectBotton_clicked()
-{
-    qDebug() << "Main Window Save Button";
-    QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
-    if(ProjectUtils::instance()->get_projects().empty())
-        return;
-    ProjectUtils::instance()->set_current_project_index(index.first().row());
-    create_json_project(ProjectUtils::instance()->get_projects().at(ProjectUtils::instance()->get_current_project_index()));
-}
-
-void MainWindow::load_fonts(){
-    QFontDatabase database;
-    database.addApplicationFont(":/fonts/fonts/Roboto-Regular.ttf");
-    database.addApplicationFont(":/fonts/fonts/Roboto-Medium.ttf");
-    database.addApplicationFont(":/fonts/fonts/Roboto-Light.ttf");
-    database.addApplicationFont(":/fonts/fonts/Roboto-Bold.ttf");
-}
-
-void MainWindow::on_pushButton_2_clicked(){
-    taskDescriptionWidget->hide();
-}
-
+/**
+ * @brief Removes, updates and restores all widgets in the task layouts for the selected project
+ */
 void MainWindow::update_ui(){
     QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
     remove_all_widgets(toDoLayout);
     remove_all_widgets(doingLayout);
     remove_all_widgets(testingLayout);
     remove_all_widgets(doneLayout);
-    if(ProjectUtils::instance()->get_projects().empty())
+    if(ProjectUtils::Instance()->get_projects().empty())
         return;
 
-    projectNameLabel->setText(QString::fromStdString(ProjectUtils::instance()->get_open_project().get_name()));
+    projectNameLabel->setText(QString::fromStdString(ProjectUtils::Instance()->get_open_project().get_name()));
     taskDescriptionWidget->hide();
 
-    vector<Task> tasks = ProjectUtils::instance()->get_projects().at(index.first().row()).get_tasks();
+    vector<Task> tasks = ProjectUtils::Instance()->get_projects().at(index.first().row()).get_tasks();
     if(!tasks.empty()) {
-        for(int i = 0; i < tasks.size(); i++){
-            ProjectUtils::instance()->open_task(tasks.at(i));
+        for(uint8_t i = 0; i < tasks.size(); i++){
+            ProjectUtils::Instance()->open_task(tasks.at(i));
             TaskWidget *task = new TaskWidget;
             if(tasks.at(i).get_status() == "To Do")
                 ui->Todo_List->addWidget(task);
@@ -263,9 +128,178 @@ void MainWindow::update_ui(){
     }
 }
 
+/**
+ * @brief Displays the detailed view of the selected task
+ * @param task : currently selected task
+ */
+void MainWindow::DisplayDetailedView(Task task) {
+    ui->taskName->setText(QString::fromStdString(task.get_name()));
+    ui->taskDueDate->setText(task.get_due_date().toString("MM/dd/yyyy"));
+    ui->taskCreatedDate->setText(task.get_created_date().toString("MM/dd/yyyy"));
+    ui->taskDescription->setText(QString::fromStdString(task.get_description()));
+    taskDescriptionWidget->show();
+}
+
+/**
+ * @brief Accessor for the editingTask flag
+ * @return editingTask : Boolean flag for whether the taskDialog is editing or creating a task
+ */
+bool MainWindow::getEditFlag()
+{
+    return editingTask;
+}
+
+/**
+ * @brief Mutator for the editingTask flag
+ * @param f : the passed boolean value
+ */
+void MainWindow::setEditFlag(bool f)
+{
+    editingTask = f;
+}
 
 
-void MainWindow::on_pushButton_clicked()
+///Slots
+/**
+ * @brief Adds the new or edited task widget to and updates the layout upon accepted taskDialog window
+ */
+void MainWindow::onTaskDialogAccepted()
+{
+    TaskWidget *task = new TaskWidget;
+    Task current = ProjectUtils::Instance()->get_open_task();
+    if(current.get_status() == "To Do")
+        ui->Todo_List->addWidget(task);
+    else if(current.get_status() == "Doing")
+        ui->Doing_List->addWidget(task);
+    else if(current.get_status() == "Testing")
+        ui->Testing_List->addWidget(task);
+    else if(current.get_status() == "Done")
+        ui->Done_List->addWidget(task);
+
+    update_ui();
+}
+
+/**
+ * @brief Creates a new project and adds it to the vector on button press
+ */
+void MainWindow::onNewProjectButtonClick()
+{
+    Project newProject("New Project", "");
+    projectModel->addProject(newProject, Qt::EditRole);
+}
+
+/**
+ * @brief Deletes the currently selected project and updates the layouts
+ */
+void MainWindow::onDeleteProjectButtonClick()
+{
+    QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
+    if(ProjectUtils::Instance()->get_projects().empty())
+        return;
+    ProjectUtils::Instance()->set_current_project_index(index.first().row());
+    remove_json_project(ProjectUtils::Instance()->get_projects().at(ProjectUtils::Instance()->get_current_project_index()).get_name());
+    projectModel->deleteProject(projectListView->selectionModel()->selectedIndexes().first(), Qt::EditRole);
+    projectListView->setCurrentIndex(projectModel->index(0,0));
+    update_ui();
+}
+
+/**
+ * @brief Instantiates the taskDialog window for creation of a new task
+ */
+void MainWindow::onNewTaskButtonClicked()
+{
+    QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
+    if(ProjectUtils::Instance()->get_projects().empty())
+        return;
+    ProjectUtils::Instance()->set_current_project_index(index.first().row());
+    setEditFlag(false);
+    taskDialog = new TaskDialog();
+    taskDialog->show();
+}
+
+/**
+ * @brief Instantiates the taskDialog window for the currently selected task
+ */
+void MainWindow::onEditTaskButtonClicked()
+{
+    QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
+    if(ProjectUtils::Instance()->get_projects().empty())
+        return;
+    ProjectUtils::Instance()->set_current_project_index(index.first().row());
+    //ProjectUtils::Instance()->get_open_task();
+    setEditFlag(true);
+    taskDialog = new TaskDialog();
+    taskDialog->show();
+}
+
+/**
+ * @brief Calls update_ui when a project is selected
+ */
+void MainWindow::item_selected_in_list(){
+    update_ui();
+}
+
+/**
+ * @brief Clears the task widget layouts for update_ui
+ * @param layout : the currently selected project's layout
+ */
+void MainWindow::remove_all_widgets(QBoxLayout* layout){
+    QLayoutItem *child;
+    while ((child = layout->takeAt(0)) != 0) {
+        delete child->widget();
+        delete child;
+    }
+}
+
+/**
+ * @brief Signal for the save_all_projects function for Json_utils
+ */
+void MainWindow::on_saveProjectsButton_clicked()
+{
+    if(ProjectUtils::Instance()->get_projects().empty())
+        return;
+    save_all_projects(ProjectUtils::Instance()->get_projects());
+}
+
+/**
+ * @brief Signal to save currently selected project
+ */
+void MainWindow::on_saveCurrentProjectBotton_clicked()
+{
+    QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
+    if(ProjectUtils::Instance()->get_projects().empty())
+        return;
+    ProjectUtils::Instance()->set_current_project_index(index.first().row());
+    create_json_project(ProjectUtils::Instance()->get_projects().at(ProjectUtils::Instance()->get_current_project_index()));
+}
+
+/**
+ * @brief Hides whatever detailed view is opened
+ */
+void MainWindow::on_closeTaskInfoButton_clicked(){
+    taskDescriptionWidget->hide();
+}
+
+/**
+ * @brief Deletes the currently selected task from the vector and updates the layouts
+ */
+void MainWindow::onDeleteTaskButtonClicked()
+{
+    QModelIndexList index = projectListView->selectionModel()->selectedIndexes();
+    if(ProjectUtils::Instance()->get_projects().empty())
+        return;
+    ProjectUtils::Instance()->set_current_project_index(index.first().row());
+    string temp_name = ProjectUtils::Instance()->get_open_task().get_name();
+    vector<Task>::iterator temp_index = ProjectUtils::Instance()->get_projects().at(index.first().row()).find_task_by_name(temp_name);
+    ProjectUtils::Instance()->get_projects().at(index.first().row()).remove_task(temp_index);
+
+    update_ui();
+}
+
+/**
+ * @brief Shows/Hides the Project list view
+ */
+void MainWindow::on_hideProjectsButton_clicked()
 {
     if (ui->frame->isHidden()){
         ui->frame->show();
@@ -273,3 +307,20 @@ void MainWindow::on_pushButton_clicked()
         ui->frame->hide();
     }
 }
+
+
+///Other  functions
+/**
+ * @brief Sets all layout fonts
+ */
+void MainWindow::load_fonts(){
+    QFontDatabase database;
+    database.addApplicationFont(":/fonts/fonts/Roboto-Regular.ttf");
+    database.addApplicationFont(":/fonts/fonts/Roboto-Medium.ttf");
+    database.addApplicationFont(":/fonts/fonts/Roboto-Light.ttf");
+    database.addApplicationFont(":/fonts/fonts/Roboto-Bold.ttf");
+}
+
+
+
+
--- MonkeyManager/mainwindow.h
+++ MonkeyManager/mainwindow.h
@@ -7,9 +7,19 @@
 #include <QListView>
 #include <QFrame>
 #include <QBoxLayout>
+#include <QDesktopWidget>
+#include <QLabel>
+#include <QPalette>
+#include <QFontDatabase>
+#include <QFont>
+#include <QPushButton>
 
-#include "taskdialog.h"
 #include "projectlistmodel.h"
+#include "ui_mainwindow.h"
+#include "taskwidget.h"
+#include "taskdialog.h"
+#include "project.h"
+#include "Json_utils.h"
 
 namespace Ui {
 class MainWindow;
@@ -20,41 +30,118 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor
+     * @param parent : QT default
+     */
     explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-    void centerAndResize();
-    void updateTaskInToDo();
-    void updateTaskInDoing();
-    void updateTaskInTesting();
-    void updateTaskInToDone();
-    void update_ui();
-    void DisplayDetailedView(Task task);
 
-    bool getEditFlag();
-    void setEditFlag(bool);
-
+    /**
+     * @brief returns the instance of MainWindow
+     * @return
+     */
     static MainWindow *getInstance();
 
+    /**
+     * @brief Deconstructor
+     */
+    ~MainWindow();
+
+    /**
+     * @brief Formats the frame based on the window size
+     */
+    void centerAndResize();
+
+    /**
+     * @brief Removes, updates and restores all widgets in the task layouts for the selected project
+     */
+    void update_ui();
+
+    /**
+     * @brief Displays the detailed view of the selected task
+     * @param task : currently selected task
+     */
+    void DisplayDetailedView(Task task);
+
+    /**
+     * @brief Accessor for the editingTask flag
+     * @return editingTask : Boolean flag for whether the taskDialog is editing or creating a task
+     */
+    bool getEditFlag();
+
+    /**
+     * @brief Mutator for the editingTask flag
+     * @param f : the passed boolean value
+     */
+    void setEditFlag(bool);
 
 public slots:
+    /**
+     * @brief Adds the new or edited task widget to and updates the layout upon accepted taskDialog window
+     */
     void onTaskDialogAccepted();
 
 private slots:
+    /**
+     * @brief Creates a new project and adds it to the vector on button press
+     */
     void onNewProjectButtonClick();
+
+    /**
+     * @brief Deletes the currently selected project and updates the layouts
+     */
     void onDeleteProjectButtonClick();
+
+    /**
+     * @brief Instantiates the taskDialog window for creation of a new task
+     */
     void onNewTaskButtonClicked();
+
+    /**
+     * @brief Instantiates the taskDialog window for the currently selected task
+     */
     void onEditTaskButtonClicked();
+
+    /**
+     * @brief Calls update_ui when a project is selected
+     */
     void item_selected_in_list();
+
+    /**
+     * @brief Clears the task widget layouts for update_ui
+     * @param layout : the currently selected project's layout
+     */
     void remove_all_widgets(QBoxLayout* layout);
-    void mousePressEvent(QMouseEvent *event);
+
+    /**
+     * @brief Signal for the save_all_projects function for Json_utils
+     */
     void on_saveProjectsButton_clicked();
+
+    /**
+     * @brief Signal to save currently selected project
+     */
     void on_saveCurrentProjectBotton_clicked();
-    void on_pushButton_2_clicked();
+
+    /**
+     * @brief Hides whatever detailed view is opened
+     */
+    void on_closeTaskInfoButton_clicked();
+
+    /**
+     * @brief Deletes the currently selected task from the vector and updates the layouts
+     */
     void onDeleteTaskButtonClicked();
 
-    void on_pushButton_clicked();
+    /**
+     * @brief Shows/Hides the Project list view
+     */
+    void on_hideProjectsButton_clicked();
 
 private:
+    /**
+     * @brief Sets all layout fonts
+     */
     void load_fonts();
 
     Ui::MainWindow *ui;
@@ -66,14 +153,10 @@ private:
     QPushButton *deleteTaskButton;
 
     QLabel *projectNameLabel;
-    QFrame *toDoFrame;
-
-    bool editingTask;
 
     QListView *projectListView;
     ProjectListModel *projectModel;
 
-    vector<Project> projectVector;
     TaskDialog *taskDialog;
 
     QBoxLayout *toDoLayout;
@@ -84,6 +167,7 @@ private:
     QWidget *taskDescriptionWidget;
 
     Project m_project;
+    bool editingTask;
 };
 
 #endif // MAINWINDOW_H
